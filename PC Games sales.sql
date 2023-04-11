571568649c1a2ed2f6eb0299c4344f928cda1306
Ahmed Sayed <90968705+AhmedSayed97@users.noreply.github.com>
2023-04-12 00:52:46 +0200

    Add files via upload

---
+++ PC Games sales.sql
@@ -0,0 +1,87 @@
+/*
+PC Games Sales
+*/
+
+
+Select * From Games
+
+---Adding a column release_date
+
+Alter Table Games
+Add Release_date date
+Update Games
+Set Release_date = Release
+
+
+---delete Release column
+
+Alter Table Games
+Drop Column Release
+
+
+---looking for Duplicates (No Duplicates Where found)
+
+With RowNumCTE As(
+Select *, ROW_NUMBER() Over(
+Partition By Name,
+             Sales,
+			 Series,
+			 Genre,
+			 Developer,
+			 Publisher
+			 Order By Name
+			 ) row_num
+From Games)
+Select * From RowNumCTE
+Where row_num > 1
+Order By row_num
+
+
+---Dealing with Null values [filling missing values with a text (Not available)]
+
+Select Name, Series, COALESCE(Series, 'Not Available') Series_edited From Games
+
+Update Games
+Set Series = COALESCE(Series, 'Not Available')
+
+---modifying Sales Column (Sales of the game in Millions)
+
+Update Games
+Set Sales = Sales*1000000
+
+---The largest Sales
+
+Select MAX(Sales) As largest_sales From Games
+
+
+---The smallest Sales
+
+Select MIN(Sales) As smallest_sales From Games
+
+---The Average of Sales
+
+Select AVG(Sales) As AVG_OF_Sales From Games
+
+---The Sum of sales
+
+Select SUM(Sales) As Sum_of_Sales From Games 
+
+---The most Popular genre? (Real-time strategy)
+
+Select Genre, COUNT(Genre) AS Genres_Count From Games
+Group By Genre
+Order By Genres_Count Desc
+
+
+--- Which publisher published most of the games? (Electronic Arts)
+
+Select Publisher, COUNT(Publisher) AS Publishers_Count From Games
+Group By Publisher
+Order By Publishers_Count Desc
+
+
+--- Which developer developed most of the games? (Blizzard Entertainment)
+
+Select Developer, COUNT(Developer) AS Developers_Count From Games
+Group By Developer
+Order By Developers_Count Desc
